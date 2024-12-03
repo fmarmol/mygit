0dec68123f0c8e2d7c73fdf4406bf6a9ef9fb11c
florent marmol <florent.marmol@gmail.com>
2024-12-03 08:16:03 +0100

    test4

--- push.go
+++ push.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"path/filepath"
-
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
 	"github.com/spf13/cobra"
@@ -20,28 +17,11 @@ var cmdPush = &cobra.Command{
 		if err != nil {
 			return err
 		}
-		// remote, err := r.Remote("origin")
-		// if err != nil {
-		// 	return nil
-		// }
-		// for _, ref := range remote.Config().Fetch {
-		// 	if ref.Match(head.Name()) {
-		// 		fmt.Println(ref, head.Name().String())
-		// 	}
-
-		// }
-		// return nil
-		// fmt.Println("HEAD TARGET:", filepath.Base(head.Name().String()), "END")
-		branchName := filepath.Base(head.Name().String())
-		_ = branchName
-		// refSpec := head.Name().String() + ":refs/remotes/origin/" + filepath.Base(head.Name().String())
 		refSpec := head.Name().String() + ":" + head.Name().String()
-		// _ = refSpec
-		fmt.Println(refSpec)
 		err = r.Push(&git.PushOptions{
 			RemoteName: "origin",
 			RefSpecs:   []config.RefSpec{config.RefSpec(refSpec)},
-			Auth:       nil, // Optional: Use for authentication, e.g., SSH keys or credentials.
+			Auth:       nil,
 		})
 		if err != nil {
 			return err
